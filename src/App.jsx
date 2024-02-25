a61bbb0ba3dbf98b56f17f10cc7db882c6b51ebe
Rabiyacesen <154918374+Rabiyacesen@users.noreply.github.com>
2024-02-25 20:33:44 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+  
+
+
+  return (
+    <>
+      <Navbar />
+      <h1>App</h1>
+    
+    
+    
+    </>
+  )
+}
+
+export default App
